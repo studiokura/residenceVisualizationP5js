436d884c294025356dd83d3bee92011ecc69bee9
Hirofumi Matsuzaki <residence@studiokura.com>
2016-01-08 23:19:38 +0900

    Update sketch.js
    静止画からanimationに

--- sketch.js
+++ sketch.js
@@ -15,7 +15,11 @@ var latitude = [];
 var longitude = [];
 var artistCountry = [];
 var cC = [];
+var cCity = [];
 var count = [0, 0];
+var circleSize = 3;
+var ic = 0;
+var cityLife = [];
 
 function preload() {
   cities = loadTable("https://www.studiokura.com/p5/cities1.csv", "header");
@@ -29,17 +33,18 @@ function setup() {
   background(255);
   fill(255, 150);
   noStroke();
+  frameRate(3);
   image(geomap, 40, 9, 600 * 2, 340 * 2);
   for (var i = 0; i < cities.getRowCount(); i += 1) {
     for (var j = 0; j < artists.getRowCount(); j += 1) {
       city[i] = cities.getString(i, "realname");
       latitude[i] = cities.getNum(i, "lat");
       longitude[i] = cities.getNum(i, "lng");
-
+      cityLife[j] = 40;
       artist[j] = artists.getString(j, "artist");
       origin[j] = artists.getString(j, "origin_location");
       destination[j] = artists.getString(j, "destination_location");
-
+      artistCountry[j] = artists.getString(j, "country");
 
       if (city[i] == origin[j]) {
         originX[j] = cities.getNum(i, "lat");
@@ -49,53 +54,93 @@ function setup() {
         destinationX[j] = cities.getNum(i, "lat");
         destinationY[j] = cities.getNum(i, "lng");
       }
-      arcSetXY(originX[j], originY[j], destinationX[j], destinationY[j])
+
+      // arcSetXY(originX[j], originY[j], destinationX[j], destinationY[j])
 
     }
-    setXY(latitude[i], longitude[i], city[i])
+    // setXY(latitude[i], longitude[i], city[i]);
+
   }
-  for (var k = 0; k < artists.getRowCount(); k += 1) {
+  /* for (var k = 0; k < artists.getRowCount(); k += 1) {
     artistCountry[k] = artists.getString(k, "country");
-
+    
   }
-
+*/
   for (var l = 0; l < countryName.getRowCount(); l += 1) {
     country[l] = countryName.getString(l, "country");
     cC[l] = 0;
-    countries(country[l], l, cC[l]);
-
+    //countries(country[l], l, cC[l]);
+    fill(0);
+    text(country[l], 10, 400 + (l * 10));
+    text(cC[l], 100, 400 + (l * 10));
   }
+}
 
-
-
-  // println(country[l]);
-
-  /*
-   for (var l = 0; l < countryName.getRowCount(); l += 1) {
-     countryName[l]=countryName.getString(l,"country");
-      //println(countryName[l]);
-  }
-  */
+function mouseClicked() {
+  // ic+=1;
 }
 
 
-
-
-
-
 function draw() {
+  fill(255);
+  rect(0, 0, 30, 200);
+  ic += 1;
+   if(ic>=artists.getRowCount()){
+    ic=0;
+     background(255);
+     image(geomap, 40, 9, 600 * 2, 340 * 2);
+     for (var l = 0; l < countryName.getRowCount(); l += 1) {
+     cC[l]=0;
+     }
+  }
+  fill(0);
+  text(ic, 10, 10);
+  text(cityLife[1], 20, 100);
+  /*for (var i = 0; i < cities.getRowCount(); i += 1) {
+   print(country[i]);
+  }
+  */
+  //println(country[2]);
 
+  arcSetXY(originX[ic], originY[ic], destinationX[ic], destinationY[ic])
+  setXY(originX[ic], originY[ic], origin[ic]);
+  fill(255);
+  rect(0, 300, 300, 400);
+  for (var l = 0; l < countryName.getRowCount(); l += 1) {
+    // for (var j = 0; j < artists.getRowCount(); j += 1) {
+    fill(0);
+    text(country[l], 10, 400 + (l * 10));
+    text(cC[l], 100, 400 + (l * 10));
+    fill(100,60);
+    rect(100, 390 + (l * 10), cC[l], 8);
+   
+    if (country[l] == artistCountry[ic]) {
+      cC[l] += 1;
+    }
+
+  }
+
+  //}
 }
 
 function setXY(lat, lng, realname) {
   var x = map(lng, -180, 180, 0, width);
   var y = map(lat, 90, -90, 0, height);
-  fill(0);
+  fill(0, 50);
   noStroke();
-  ellipse(x, y, 3, 3);
+  ellipse(x, y, circleSize, circleSize);
+  fill(0, 50);
 
-  fill(0, 60);
   text(realname, x, y);
+}
+
+function getCircle(lat, lng) {
+  x = lat;
+  y = lng;
+  var d = dist(mouseX, mouseY, x, y);
+  if (d < circleSize && mousePressed) {
+    circleSize(199);
+  }
 }
 
 
@@ -111,18 +156,18 @@ function arcSetXY(lat, lng, lat2, lng2) {
   stroke(255, 0, 0, 50);
   bezierVertex(x, y - d / 2.1, x2, y2 - d / 2.1, x2, y2);
   endShape();
+
 }
 
 
 function countries(name, y, counter) {
   fill(0);
   text(name, 20, 370 + (13 * y));
- 
+
   for (var i = 0; i < artists.getRowCount(); i += 1) {
     if (name == artistCountry[i]) {
-      counter+=1;
-      print(counter);
+      counter += 1;
     }
   }
- text(counter, 110, 370 + (13 * y));
+  text(counter, 110, 370 + (13 * y));
 }
